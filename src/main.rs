f37fd2a49e33009ac0edf175b2512313691fb326
valadaptive <valadaptive@protonmail.com>
2022-01-25 03:45:34 -0500

    I/O functionality

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
+use std::cell::RefCell;
 use std::fs::File;
+use std::io::{Stdin, Stdout, stdout};
 use std::io::prelude::*;
 use std::env::args;
-use std::ops::Index;
-use std::ops::IndexMut;
+use std::io::stdin;
 
 mod bit_twiddling;
 mod opcode;
 
-use byteorder::LittleEndian;
+use byteorder::{WriteBytesExt, BigEndian};
 use byteorder::ReadBytesExt;
 
 use crate::bit_twiddling::*;
 use crate::opcode::*;
 
-const MEMORY_SIZE: usize = u16::MAX as usize;
+const MEMORY_SIZE: usize = (u16::MAX as usize) + 1;
 
 /// Program status register.
 /// `psr[15]` is 1 if running in user mode, 0 if in supervisor mode.
@@ -21,21 +22,84 @@ const MEMORY_SIZE: usize = u16::MAX as usize;
 /// `psr[2:0]` holds condition codes (set depending on whether the previous result was positive, negative, or zero)
 const PSR: u16 = 0xFFFC;
 
+/// Machine control register.
+/// When the top bit is cleared, the emulator exits.
+const MCR: u16 = 0xFFFE;
+
 struct Memory {
-    memory: [u16; MEMORY_SIZE]
+    memory: [u16; MEMORY_SIZE],
+    keyboard_io: RefCell<KeyboardIO>,
+    stdout: Stdout
 }
 
-impl Index<u16> for Memory {
-    type Output = u16;
+impl Memory {
+    fn get(&self, addr: u16) -> u16 {
+        match addr {
+            // Keyboard status register
+            0xFE00 => self.keyboard_io.borrow_mut().read_kbsr(),
+            // Keyboard data register
+            0xFE02 => self.keyboard_io.borrow_mut().read_kbdr(),
+            0xFE04 => 0x8000,
+            _ => self.memory[addr as usize]
+        }
+    }
 
-    fn index(&self, index: u16) -> &Self::Output {
-        &self.memory[index as usize]
+    fn set(&mut self, addr: u16, value: u16) {
+        match addr {
+            // Ignore writes into status/read-only registers
+            0xFE00 | 0xFE02 | 0xFE04 => (),
+            // Display data register
+            0xFE06 => {
+                self.stdout.write_u8((value & 0xFF) as u8);
+                self.stdout.flush();
+            },
+            _ => {
+                self.memory[addr as usize] = value;
+            }
+        };
     }
 }
 
-impl IndexMut<u16> for Memory {
-    fn index_mut(&mut self, index: u16) -> &mut Self::Output {
-        &mut self.memory[index as usize]
+struct KeyboardIO {
+    need_more_input: bool,
+    kbsr: bool,
+    kbdr: u16,
+    stdin: Stdin
+}
+
+impl KeyboardIO {
+    fn new(stdin: Stdin) -> Self {
+        KeyboardIO {
+            need_more_input: true,
+            kbsr: false,
+            kbdr: 0,
+            stdin
+        }
+    }
+
+    fn update_input(&mut self) {
+        if let Ok(keycode) = self.stdin.read_u8() {
+            self.kbdr = keycode as u16;
+            self.kbsr = true;
+            self.need_more_input = false;
+        }
+    }
+
+    fn read_kbsr(&mut self) -> u16 {
+        if self.need_more_input {
+            self.update_input();
+        }
+
+        if self.kbsr {0x8000} else {0x0000}
+    }
+
+    fn read_kbdr(&mut self) -> u16 {
+        if self.need_more_input {
+            self.update_input();
+        }
+
+        self.need_more_input = true;
+        self.kbdr
     }
 }
 
@@ -53,25 +117,32 @@ pub struct Cpu {
     /// The saved user mode stack pointer
     saved_usp: u16,
     /// The saved supervisor mode stack pointer
-    saved_ssp: u16
+    saved_ssp: u16,
 }
 
 impl Cpu {
-    fn new() -> Self {
+    fn new(stdin: Stdin, stdout: Stdout) -> Self {
+        let mut mem_raw = [0u16; MEMORY_SIZE];
+        // Initialize MCR so we don't halt immediately
+        mem_raw[MCR as usize] = 0xFFFF;
         Cpu {
             registers: [0u16; 8],
-            memory: Memory {memory: [0u16; MEMORY_SIZE]},
+            memory: Memory {
+                memory: mem_raw,
+                keyboard_io: RefCell::new(KeyboardIO::new(stdin)),
+                stdout
+            },
             pc: 0u16,
             // We decrement the stack pointer before writing and increment after reading, so these start at 1 after the
             // addresses at which the stack actually begins. Stacks grow downwards in memory.
             saved_ssp: 0x3000,
-            saved_usp: 0xFE00
+            saved_usp: 0xFE00,
         }
     }
 
     fn address_accessible(&self, addr: u16) -> bool {
         // Only protect memory if not in privileged mode
-        if get_bits::<15, 15>(self.memory[PSR]) == 0 {
+        if get_bits::<15, 15>(self.memory.get(PSR)) == 0 {
             return true;
         }
 
@@ -79,18 +150,21 @@ impl Cpu {
     }
 
     fn enter_supervisor_mode(&mut self) {
-        let old_psr = self.memory[PSR];
-        if get_bits::<15, 15>(self.memory[PSR]) == 1 {
+        let old_psr = self.memory.get(PSR);
+        if get_bits::<15, 15>(self.memory.get(PSR)) == 1 {
             // Switch from the user stack pointer to the system stack pointer
             self.saved_usp = self.registers[6];
             self.registers[6] = self.saved_ssp;
-            self.memory[PSR] &= !(1 << 15);
+            self.memory.set(PSR, self.memory.get(PSR) & !(1 << 15));
         }
-        // Push old PSR and PC to stack
-        self.registers[6] -= 1;
-        self.memory[self.registers[6]] = old_psr;
-        self.registers[6] -= 1;
-        self.memory[self.registers[6]] =  self.pc;
+
+        #[cfg(feature = "by_the_book")] {
+            // Push old PSR and PC to stack
+            self.registers[6] -= 1;
+            self.memory.set(self.registers[6], old_psr);
+            self.registers[6] -= 1;
+            self.memory.set(self.registers[6], self.pc);
+        }
     }
 
     fn handle_exception(&mut self, exception_vector: u8) {
@@ -109,12 +183,12 @@ impl Cpu {
     }
 
     fn get_priority_level(&self) -> u16 {
-        return (self.memory[PSR] >> 8) & 0b111;
+        return (self.memory.get(PSR) >> 8) & 0b111;
     }
 
     fn set_priority_level(&mut self, level: u16) {
         assert!(level <= 7);
-        self.memory[PSR] = (self.memory[PSR] & !0b11100000000) | (level << 8);
+        self.memory.set(PSR, (self.memory.get(PSR) & !0b11100000000) | (level << 8));
     }
 
     fn get_reg_hi(&self, instruction: u16) -> u16 {
@@ -139,7 +213,7 @@ impl Cpu {
                 let result = match opcode {
                     Opcode::Add | Opcode::And => {
                         let src_value_2 = if get_bits::<5, 5>(instruction) == 1 { // immediate mode
-                            get_bits::<0, 4>(instruction)
+                            sign_extend::<5>(get_bits::<0, 4>(instruction) as i16) as u16
                         } else {
                             self.registers[get_bits::<0, 2>(instruction) as usize]
                         };
@@ -164,7 +238,7 @@ impl Cpu {
                                     self.handle_exception(0x02);
                                     return;
                                 }
-                                self.memory[addr]
+                                self.memory.get(addr)
                             },
                             // load indirect
                             Opcode::Ldi => {
@@ -172,12 +246,12 @@ impl Cpu {
                                     self.handle_exception(0x02);
                                     return;
                                 }
-                                let indirect_addr = self.memory[addr];
+                                let indirect_addr = self.memory.get(addr);
                                 if !self.address_accessible(addr) {
                                     self.handle_exception(0x02);
                                     return;
                                 }
-                                self.memory[indirect_addr]
+                                self.memory.get(indirect_addr)
                             },
                             _ => unreachable!()
                         }
@@ -190,7 +264,7 @@ impl Cpu {
                             self.handle_exception(0x02);
                             return;
                         }
-                        self.memory[addr]
+                        self.memory.get(addr)
                     },
 
                     Opcode::Not => {
@@ -202,18 +276,18 @@ impl Cpu {
 
                 self.registers[get_bits::<9, 11>(instruction) as usize] = result;
                 // Replace lower 3 bits of PSR with the new condition bits
-                self.memory[PSR] = (self.memory[PSR] & !0b111) | match (result as i16).signum() {
+                self.memory.set(PSR, (self.memory.get(PSR) & !0b111) | match (result as i16).signum() {
                     -1 => COND_NEGATIVE,
                     0 => COND_ZERO,
                     1 => COND_POSITIVE,
                     _ => unreachable!()
-                }
+                });
             },
 
             Opcode::Br => {
                 let nzp = get_bits::<9, 11>(instruction);
                 // This will only match the lowest 3 bits, which store the condition codes
-                if (nzp & self.memory[PSR]) > 0 {
+                if (nzp & self.memory.get(PSR)) > 0 {
                     let pc_offset = sign_extend::<9>(get_bits::<0, 8>(instruction) as i16);
                     self.pc = u16::wrapping_add(self.pc, pc_offset as u16);
                 }
@@ -227,7 +301,7 @@ impl Cpu {
                 // The only other way to do this is to modify the PSR directly.
                 // Commenting this out for now because I have no idea about the specifics of its functionality.
                 /* if get_bits::<0, 0>(instruction) == 1 {
-                    if get_bits::<15, 15>(self.memory[PSR]) == 1 {
+                    if get_bits::<15, 15>(self.memory.get(PSR)) == 1 {
                         // Tried to execute JMPT/RTT from user mode--trigger a privilege mode violation
                         // TODO: do the emulators actually do this? do they even check the privilege bit at all?
                         // how has no one actually fully implemented this architecture?
@@ -236,7 +310,7 @@ impl Cpu {
                     }
 
                     // Set user-mode PSR bit
-                    self.memory[PSR] |= 1 << 15;
+                    self.memory.get(PSR) |= 1 << 15;
                 } */
             },
 
@@ -267,13 +341,13 @@ impl Cpu {
 
                 // read address from memory if indirect store
                 if let Opcode::Sti = opcode {
-                    addr = self.memory[addr];
+                    addr = self.memory.get(addr);
                     if !self.address_accessible(addr) {
                         self.handle_exception(0x02);
                     }
                 }
 
-                self.memory[addr] = self.get_reg_hi(instruction);
+                self.memory.set(addr, self.get_reg_hi(instruction));
             },
 
             Opcode::Str => {
@@ -283,26 +357,26 @@ impl Cpu {
                     self.handle_exception(0x02);
                     return;
                 }
-                self.memory[addr] = self.get_reg_hi(instruction);
+                self.memory.set(addr, self.get_reg_hi(instruction));
             },
 
             Opcode::Rti => {
-                if get_bits::<15, 15>(self.memory[PSR]) == 1 {
+                if get_bits::<15, 15>(self.memory.get(PSR)) == 1 {
                     // Tried to execute RTI from user mode--trigger a privilege mode violation
                     self.handle_exception(0x00);
                     return;
                 }
 
                 // Restore PC from supervisor stack pointer
-                self.pc = self.memory[self.registers[6]];
+                self.pc = self.memory.get(self.registers[6]);
                 self.registers[6] += 1;
 
                 // TODO: the book says to pop the system stack before restoring the PSR. Why?
-                let new_psr = self.memory[self.registers[6]];
+                let new_psr = self.memory.get(self.registers[6]);
                 self.registers[6] += 1;
-                self.memory[PSR] = new_psr;
+                self.memory.set(PSR, new_psr);
 
-                if get_bits::<15, 15>(self.memory[PSR]) == 1 {
+                if get_bits::<15, 15>(self.memory.get(PSR)) == 1 {
                     // We are now back in user mode
                     self.saved_ssp = self.registers[6];
                     self.registers[6] = self.saved_usp;
@@ -316,29 +390,191 @@ impl Cpu {
             Opcode::Trap => {
                 self.enter_supervisor_mode();
 
+                // It's not documented anywhere in the book, but TRAP sets R7 to the previous PC
+                #[cfg(not(feature = "by_the_book"))] {
+                    self.registers[7] = self.pc;
+                }
+
                 // Jump into code specified by trap vector table
-                // TODO: does this mean we don't increment the instruction pointer?
-                self.pc = self.memory[get_bits::<0, 7>(instruction)];
+                self.pc = self.memory.get(get_bits::<0, 7>(instruction));
             }
         }
     }
+
+    fn step(&mut self) {
+        let instruction = self.memory.get(self.pc);
+        // println!("PC: {:#06x}, instruction: {:#06x} ({})", self.pc, instruction, disassemble_instruction(instruction));
+        self.pc = self.pc.wrapping_add(1);
+        let op = get_bits::<12, 15>(instruction);
+        match op {
+            0 => self.execute_instruction::<0>(instruction),
+            1 => self.execute_instruction::<1>(instruction),
+            2 => self.execute_instruction::<2>(instruction),
+            3 => self.execute_instruction::<3>(instruction),
+            4 => self.execute_instruction::<4>(instruction),
+            5 => self.execute_instruction::<5>(instruction),
+            6 => self.execute_instruction::<6>(instruction),
+            7 => self.execute_instruction::<7>(instruction),
+            8 => self.execute_instruction::<8>(instruction),
+            9 => self.execute_instruction::<9>(instruction),
+            10 => self.execute_instruction::<10>(instruction),
+            11 => self.execute_instruction::<11>(instruction),
+            12 => self.execute_instruction::<12>(instruction),
+            13 => self.execute_instruction::<13>(instruction),
+            14 => self.execute_instruction::<14>(instruction),
+            15 => self.execute_instruction::<15>(instruction),
+            _ => unreachable!()
+        };
+    }
+}
+
+fn read_program_to_cpu(mut file: File, cpu: &mut Cpu) -> std::io::Result<()> {
+    let mut buf = Vec::new();
+    let origin = file.read_u16::<BigEndian>()? as usize;
+    file.read_to_end(&mut buf)?;
+    buf.chunks(2)
+        .enumerate()
+        .for_each(|(i, chunk)| {
+            cpu.memory.memory[i + origin] = ((chunk[0] as u16) << 8) | (chunk[1] as u16)
+        });
+    Ok(())
+}
+
+fn disassemble_instruction(instruction: u16) -> String {
+    let mut parts = Vec::<String>::new();
+    let opcode = Opcode::from_int(get_bits::<12, 15>(instruction) as u8);
+    match opcode {
+        Opcode::Add | Opcode::And => {
+            parts.push(match opcode {
+                Opcode::Add => "ADD",
+                Opcode::And => "AND",
+                _ => unreachable!()
+            }.to_string());
+
+            parts.push(format!("R{}", get_bits::<9, 11>(instruction)));
+            parts.push(format!("R{}", get_bits::<6, 8>(instruction)));
+
+            if get_bits::<5, 5>(instruction) == 1 {
+                parts.push(sign_extend::<5>(get_bits::<0, 4>(instruction) as i16).to_string());
+            } else {
+                parts.push(format!("R{}", get_bits::<0, 2>(instruction)));
+            }
+        },
+
+        Opcode::Not => {
+            parts.push("NOT".to_string());
+            parts.push(format!("R{}", get_bits::<9, 11>(instruction)));
+            parts.push(format!("R{}", get_bits::<6, 8>(instruction)));
+        }
+
+        Opcode::Br => {
+            let nzp = get_bits::<9, 11>(instruction);
+            let pc_offset = sign_extend::<9>(get_bits::<0, 8>(instruction) as i16);
+            parts.push(format!("BR{}{}{}",
+                if nzp & 0b100 == 0 {""} else {"n"},
+                if nzp & 0b010 == 0 {""} else {"z"},
+                if nzp & 0b001 == 0 {""} else {"p"},
+            ));
+            parts.push(format!("{:#06x}", pc_offset));
+        },
+
+        Opcode::Jmp => {
+            let base_register = get_bits::<6, 8>(instruction);
+            parts.push(if base_register == 7 {"RET"} else {"JMP"}.to_string());
+            if base_register != 7 {
+                parts.push(format!("R{}", base_register));
+            }
+        },
+
+        Opcode::Jsr => {
+            if get_bits::<11, 11>(instruction) == 1 {
+                // JSR: PC-relative
+                let pc_offset = sign_extend::<11>(get_bits::<0, 10>(instruction) as i16);
+                parts.push("JSR".to_string());
+                parts.push(format!("{:#06x}", pc_offset));
+            } else {
+                // JSRR: absolute
+                parts.push("JSRR".to_string());
+                parts.push(format!("R{}", get_bits::<6, 8>(instruction)));
+            }
+        },
+
+        Opcode::Ld | Opcode::Ldi | Opcode::Lea | Opcode::St | Opcode::Sti => {
+            let pc_offset = sign_extend::<9>(get_bits::<0, 8>(instruction) as i16);
+
+            parts.push(match opcode {
+                // load effective address: just return the address
+                Opcode::Lea => "LEA",
+                // load direct
+                Opcode::Ld => "LD",
+                // load indirect
+                Opcode::Ldi => "LDI",
+                Opcode::St => "ST",
+                Opcode::Sti => "STI",
+                _ => unreachable!()
+            }.to_string());
+
+            parts.push(format!("R{}", get_bits::<9, 11>(instruction)));
+
+            parts.push(format!("{:#06x}", pc_offset));
+        },
+
+        Opcode::Ldr | Opcode::Str => {
+            parts.push(match opcode{
+                Opcode::Ldr => "LDR",
+                Opcode::Str => "STR",
+                _ => unreachable!()
+            }.to_string());
+            parts.push(format!("R{}", get_bits::<9, 11>(instruction)));
+            parts.push(format!("R{}", get_bits::<6, 8>(instruction)));
+            let offset = sign_extend::<6>(get_bits::<0, 5>(instruction) as i16);
+            parts.push(format!("{:#06x}", offset));
+        },
+
+        Opcode::Rti => {
+            parts.push("RTI".to_string());
+        },
+
+        Opcode::Reserved => {
+            parts.push("[reserved]".to_string());
+        }
+
+        Opcode::Trap => {
+            parts.push("TRAP".to_string());
+            let trap_vector = get_bits::<0, 7>(instruction) as u8;
+            parts.push(format!("{:#04x}", trap_vector));
+        }
+    }
+
+    parts.join(" ")
 }
 
 fn main() -> anyhow::Result<()> {
     let args = args().collect::<Vec<String>>();
-    if args.len() != 2 {
+    if args.len() != 3 {
         return Err(anyhow::anyhow!("Invalid arguments"));
     }
 
-    let mut file = File::open(&args[1])?;
-    let mut cpu_state = Cpu::new();
-    let mut buf: [u8; MEMORY_SIZE * 2] = [0u8; MEMORY_SIZE * 2];
-    let origin = file.read_u16::<LittleEndian>()? as usize;
-    file.read(&mut buf)?;
-    buf.chunks(2)
-        .enumerate()
-        .for_each(|(i, chunk)| {
-            cpu_state.memory.memory[i + origin] = ((chunk[0] as u16) << 8) | (chunk[1] as u16)
-        });
+    let os = File::open(&args[1])?;
+    let pgm = File::open(&args[2])?;
+
+    crossterm::terminal::enable_raw_mode()?;
+
+    let stdin = stdin();
+    let stdout_v = stdout();
+    let mut cpu = Cpu::new(stdin, stdout_v);
+    read_program_to_cpu(os, &mut cpu)?;
+    read_program_to_cpu(pgm, &mut cpu)?;
+
+    cpu.pc = 0x0200;
+    loop {
+        cpu.step();
+
+        // Exit once the machine control register says to
+        if (cpu.memory.get(MCR) & (1 << 15)) == 0 {
+            break;
+        }
+    }
+
     Ok(())
 }
